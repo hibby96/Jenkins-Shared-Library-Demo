d7fa75e2bcc8a1346f2cbffe86bc2f4ffd693cba
hibby96 <69158294+hibby96@users.noreply.github.com>
2020-12-17 15:44:33 -0500

    Update step1.groovy

--- vars/step1.groovy
+++ vars/step1.groovy
@@ -1,5 +1,6 @@
 #!/usr/bin/env groovy
 def call(String name = 'human') {
-   println "If you're seeing this, it means that the unapproved method worked just fine."
-   println currentBuild.getDurationString()
+   // println "If you're seeing this, it means that the unapproved method worked just fine."
+   // println currentBuild.getDurationString()
+   getLastBuild()
 }
